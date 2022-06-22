7af980a45c158b03d31c4221950c95e0b50379fb
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-21 21:11:55 -0300

    Some changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { Event } from "./pages/Event"
 
 function App() {
   return (
-    <div>
-      <Event />
-    </div>
+    <Event />
   )
 }
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -27,7 +27,7 @@ export function Sidebar() {
   const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
 
   return (
-    <div className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-weight-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
@@ -49,6 +49,6 @@ export function Sidebar() {
 
 
       </div>
-    </div>
+    </aside>
   );
 }
