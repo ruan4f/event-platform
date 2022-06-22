2a8fa9fbe878801c305201d79f8d74f36ecb75b5
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-22 20:18:27 -0300

    Create the Video Compoent

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { client } from './lib/apollo'
 
 import './styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client} >
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
