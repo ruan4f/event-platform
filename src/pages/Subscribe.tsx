e2d216efd39c67ad3ce1338c14c3957bc6c7d6d9
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-06-23 22:06:55 -0300

    Subscribe page

---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,11 @@
+export function Subscribe() {
+  return (
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+      <div className="max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+
+      </div>
+
+      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+    </div>
+  );
+}
